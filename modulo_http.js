3426cdb2dcf4a06c0bebcd33747defe9edbfa7e7
KevinAGC <101078464+KevinAGC@users.noreply.github.com>
2023-04-19 09:32:01 -0600

    Update modulo_http.js

--- modulo_http.js
+++ modulo_http.js
@@ -14,6 +14,6 @@ const server = http.createServer((req,res)=>{
     res.end('Hola mundo\n');
 });
 
-server.listen(post,hostname, ()=>{
-    console.log(`El servidor se esta ejecutando en http://${hostname}:$port/`);
+server.listen(port,hostname, ()=>{
+    console.log(`El servidor se esta ejecutando en http://${hostname}:${port}/`);
 })
